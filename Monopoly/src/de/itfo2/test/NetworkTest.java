81505fe60362e59111c811316de613c1987fd684
Arne <Arne>
2013-10-07 16:51:02 +0200

    Testklassen ersetzt

--- Monopoly/src/de/itfo2/test/NetworkTest.java
+++ Monopoly/src/de/itfo2/test/NetworkTest.java
@@ -8,14 +8,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.WindowConstants;
 
-import de.itfo2.objects.Spieler;
 import de.itfo2.event.EventBus;
 import de.itfo2.event.WuerfelEvent;
-import de.itfo2.event.WuerfelEventListener;
+import de.itfo2.event.listeners.WuerfelEventListener;
+import de.itfo2.objects.Spieler;
 
 public class NetworkTest {
 	public static void main(String[] args) throws InterruptedException {
@@ -29,13 +27,13 @@ public class NetworkTest {
 		bus.addWuerfelEventListener(new WuerfelEventListener() {
 			@Override
 			public void onEvent(WuerfelEvent event) {
-				System.out.println(event.getTokenizer().getToken(event)+"\n");
+				System.out.println(event.getPlayer() + " würfelt " + event.getValue() + "\n");
 			}
 		});
-		
+
 		btn.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent me) {
-				bus.sinkClientEvent(new WuerfelEvent(new Spieler(tbx.getText(), 100, Color.BLUE),sld.getValue()));
+				bus.sinkClientEvent(new WuerfelEvent(tbx.getText(), sld.getValue()));
 			}
 		});
 		tbx.setColumns(10);
@@ -45,13 +43,5 @@ public class NetworkTest {
 		testramen.add(pnl);
 		testramen.setSize(800, 600);
 		testramen.setVisible(true);
-		testramen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-
-		// while(true){
-		// System.out.println("sinkClientEvent...");
-		// bus.sinkClientEvent(new WuerfelEvent(new Spieler("clinkz"), 4));
-		// Thread.sleep(4000);
-		// }
 	}
 }
