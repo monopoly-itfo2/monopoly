41072179b8da4d53133d51fa8c904c4641d7edcb
Lokaler Benutzer <Lokaler Benutzer@R-166-13.Schule.107842.mts-schule.de>
2013-11-11 10:40:32 +0100

    test

--- Monopoly/src/de/itfo2/test/NetworkTest.java
+++ Monopoly/src/de/itfo2/test/NetworkTest.java
@@ -1,43 +1,65 @@
 package de.itfo2.test;
 
+import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.List;
 
+import javax.naming.spi.DirStateFactory.Result;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
+import javax.xml.transform.Templates;
 
 import de.itfo2.event.EventBus;
 import de.itfo2.event.WuerfelEvent;
 import de.itfo2.event.listeners.WuerfelEventListener;
+import de.itfo2.network.Connector;
+import de.itfo2.objects.Spieler;
 
 public class NetworkTest {
 	public static void main(String[] args) throws InterruptedException {
 		final EventBus bus = EventBus.getInstance();
+		final Connector con = Connector.getInstance();
+
 		JPanel pnl = new JPanel();
 		JFrame testramen = new JFrame();
-		final JTextField tbx = new JTextField();
-		final JSlider sld = new JSlider(1, 12);
+		final JTextField tbx = new JTextField("Marco");
+//		final JSlider sld = new JSlider(1, 12);
 		JButton btn = new JButton("sink Event");
+		final JTextArea area = new JTextArea();
+		
+//		bus.addWuerfelEventListener(new WuerfelEventListener() {
+//			@Override
+//			public void onEvent(WuerfelEvent event) {
+//				System.out.println(event.getPlayer() + " würfelt " + event.getValue() + "\n");
+//			}
+//		});
 
 		bus.addWuerfelEventListener(new WuerfelEventListener() {
 			@Override
 			public void onEvent(WuerfelEvent event) {
 				System.out.println(event.getPlayer() + " wuerfelt " + event.getValue() + "\n");
 			}
-		});
 
-		btn.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent me) {
-				bus.sinkClientEvent(new WuerfelEvent(tbx.getText(), sld.getValue()));
+			private	String asString(List<Spieler> liste) {
+				String result= "";
+				for (int i = 0; i < liste.size(); i++) {
+					result = result + liste.get(i).getName() + " ; ";
+				}
+				return result;
 			}
 		});
 		tbx.setColumns(10);
+		area.setColumns(20);
+		area.setRows(20);
 		pnl.add(tbx);
-		pnl.add(sld);
+		pnl.add(area);
 		pnl.add(btn);
 		testramen.add(pnl);
 		testramen.setSize(800, 600);
