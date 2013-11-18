2d297cb652c3ade77f3ffe6c0083e1b303e91b63
Lokaler Benutzer <Lokaler Benutzer@R-166-15.Schule.107842.mts-schule.de>
2013-11-18 10:40:48 +0100

    Test

--- Monopoly/src/de/itfo2/objects/cards/Gemeinschaftskarte01.java
+++ Monopoly/src/de/itfo2/objects/cards/Gemeinschaftskarte01.java
@@ -19,5 +19,6 @@ public class Gemeinschaftskarte01 extends Karte{
         System.out.println(text);
         MonopolyGUI.getInstance().createPopupDialog(text);
         MonopolyGUI.getInstance().addLogMessage(Verwalter.getInstance().getCurSpieler().getName() +": "+ text);
+        
     }
 }
